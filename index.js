781539a046625edf80a6edd5f87dbfba5373170f
joshua <joshua@nabu.io>
2023-10-06 11:04:53 +0000

    Initialisation du projet et ajout des dépendances

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/',(request, response) => {
+    response.status(200).json({ message:"Nothing to see here ;)" })
+})
+app.use((request, response) => {
+    response.status(404);
+    response.json({ message:"Resource not found" });
+})
+app.use((request, response) => {
+    response.status(500);
+    response.json({ message:"Oops... Something went wrong" });
+})
+
+app.listen(process.env.APP_PORT, async () => {
+    console.log(`Server is running at http://localhost:${process.env.APP_PORT}`);
+})
